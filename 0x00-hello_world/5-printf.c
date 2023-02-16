72cbcb1773ee2bcc779456ddd8a23e299e066550
davy-le-roi <mbutodavy@gmail.com>
2023-02-16 05:46:02 -0800

    This is going down for real

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ * Return: Must  always returns (0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
